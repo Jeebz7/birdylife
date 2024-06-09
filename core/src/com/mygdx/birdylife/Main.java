8202d8e3d0094e987d5904ed48dd754356f5025d
Jeebz7 <129012221+Jeebz7@users.noreply.github.com>
2024-06-08 23:30:39 -0400

    Completed Plane
    Finished plane spawning and movement mechanic

--- core/src/com/mygdx/birdylife/Main.java
+++ core/src/com/mygdx/birdylife/Main.java
@@ -1,33 +1,71 @@
 package com.mygdx.birdylife;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-	// long elapsedTime = TimeUtils.timeSinceMillis(startTime);
+	long start = System.currentTimeMillis();
 	SpriteBatch batch;
 	Texture img;
 	Texture imgPlane;
 	Player player;
-	Plane[] planes;
-	int spacingPlanes;
+	ArrayList<Plane> planes = new ArrayList<>();
+	//Plane[] planes = new Plane[8];
+	double clock;
+	double diffClock;
+	double diffTimer = 8;
+	double diffVal = 100;
+	double planeRate = 4.0;
+	int activeCount = 0;
+	//int diffValue = 100;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("bird.png");
-		imgPlane = new Texture("plane.png");
 		player = new Player(img);
+		imgPlane = new Texture("plane.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
+		//System.out.println(System.currentTimeMillis() - start);
 		batch.begin();
 		player.Draw(batch);
+		clock += Gdx.graphics.getDeltaTime();
+		diffClock += Gdx.graphics.getDeltaTime();
+		//diffTimer += Gdx.graphics.getDeltaTime();
+		for (int i = 0; i < planes.size(); i++)
+		{
+			// if (!(planes.get(i).preUpdate(batch)))
+			// {
+			// 	planes.remove(i);
+			// }
+			// else
+			// {
+				planes.get(i).Draw(batch);
+			// }
+		}
+		if (clock > planeRate)
+		{
+			double speed = Math.random() * diffVal + 150;
+			Plane temp = new Plane(imgPlane, speed);
+			planes.add(temp);
+			System.out.println(clock);
+			clock = 0;
+		}
+		if ((diffTimer < diffClock) && (diffVal <= 2000))
+		{
+			diffVal += 80;
+			planeRate *= .8;
+			diffClock = 0;
+		}
 		batch.end();
-		// System.out.println(elapsedTime);
 	}
 	
 	@Override
--- core/src/com/mygdx/birdylife/Plane.java
+++ core/src/com/mygdx/birdylife/Plane.java
@@ -1,22 +1,47 @@
 package com.mygdx.birdylife;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 
 public class Plane {
     public Vector2 position;
     public Sprite sprite;
-    public Plane(Vector2 positionVal, Texture img)
-    {
-        position = positionVal;
-        sprite = new Sprite(img);
-        sprite.setScale(4);
-    }
-    public void Draw(Sprite batch)
-    {
-        sprite.setPosition(position.x, position.y);
-    }
+    public float speed;
+    public boolean active;
 
+    public Plane(Texture img, double speedVal)
+    {
+        float f = (float) Math.random();
+        position = new Vector2(Gdx.graphics.getWidth() - 100, f * Gdx.graphics.getHeight());
+        sprite = new Sprite(img);
+        sprite.setScale(1);
+        active = true;
+        speed = (float) speedVal;
+    }
+    // public void changeSpeed(float spd)
+    // {
+    //     speed = spd;
+    // }
+    public void Draw(SpriteBatch batch)
+    {
+        Update(Gdx.graphics.getDeltaTime());
+        sprite.setPosition(position.x, position.y);
+        sprite.draw(batch);
+    }
+    public boolean preUpdate(SpriteBatch batch)
+    {
+        if (position.x < -20)
+        {
+            return false;
+        }
+        return true;
+    }
+    public void Update(float deltaTime)
+    {
+        position.x -= deltaTime*speed;
+    }
 }
